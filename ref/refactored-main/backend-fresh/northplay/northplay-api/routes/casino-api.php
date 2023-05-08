f26024dd342f6c9d0e048095b209b683780f7ddf
wrccwesta <132810742+wrccwesta@users.noreply.github.com>
2023-05-08 13:05:28 +0000

    changes

--- ref/refactored-main/backend-fresh/northplay/northplay-api/routes/casino-api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Http;
-
--- ref/refactored-main/backend-fresh/northplay/northplay-api/src/NorthplayApi.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Northplay\NorthplayApi;
-
-class NorthplayApi
-{
-}
